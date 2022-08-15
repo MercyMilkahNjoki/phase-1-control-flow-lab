d8e3855f54b41cd2b70f5128e99b9b8fdc7f90b4
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-15 22:28:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return 'This one is on me!'
+  }
+  else if (someValue > 2000 && someValue<=2500){
+      return 'I will gladly take your thirty bucks.'
+  }
+  else
+    return 'No can do.'
+  }
+// scuberGreetingForFeet(199);
+// scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC"? 'Ok, sounds good.':'No go.')
+}
+console.log(ternaryCheckCity('NYC'));
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+     return 'Bye.';
+  }
 }
